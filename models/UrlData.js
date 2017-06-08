92db4c0d022a8b71ebb6066ff7db59a7753dc6ae
vinnya3 <vincent.aceto@gmail.com>
2017-06-08 12:44:56 -0400

    feat: add files for refactor

--- models/UrlData.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const {Schema} = mongoose
-
-const urlDataSchema = new Schema({
-  url: String,
-  shortenedUrl: String
-})
-
-module.exports = mongoose.model('UrlData', urlDataSchema)
